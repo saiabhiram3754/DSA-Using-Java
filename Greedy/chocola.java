063dcda1900d3a777d3387102a03c526ef94aaea
saiabhiram3754 <saiabhiram29@gmail.com>
2026-01-24 19:38:21 +0530

    Updated Chocola
    Changed Class Name

--- Greedy/chocola.java
+++ Greedy/chocola.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Chocola {
+public class chocola {
     public static void main(String[] args) {
         int n = 4, m = 6;
         Integer costVer[] = {2, 1, 3, 1, 4};
