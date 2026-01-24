ebf81ab5376edf9c43cd6b84e614e6b1033c5926
saiabhiram3754 <saiabhiram29@gmail.com>
2026-01-24 19:26:18 +0530

    Count_Vowels

---
+++ string/Countvowels.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Countvowels {
+    public static void main (String[] args){
+        System.out.println("Enter your string:");
+        String str = new Scanner(System.in).next();
+        int count = 0;
+
+        for(int i=0; i<str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch == 'a' ||ch =='e'||ch == 'i' || ch == 'o' || ch == 'u' ){
+                count ++;
+            }
+        }
+        System.out.println("Total number of vowels: " + count);
+   }
+}
