d06ee91c46c61355f4daf88006baa3ec8b1d5ab6
saiabhiram3754 <saiabhiram29@gmail.com>
2026-01-26 13:22:28 +0530

    Integer to Roman

---
+++ Array/Integer_to_Roman.java
@@ -0,0 +1,39 @@
+package Array;
+
+public class Integer_to_Roman{
+
+    public static String intToRoman(int num) {
+        int[] values = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4,
+            1
+        };
+
+        String[] symbols = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV",
+            "I"
+        };
+
+        StringBuilder roman = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                roman.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+
+        return roman.toString();
+    }
+
+    public static void main(String[] args) {
+        int num = 3749;
+        String result = intToRoman(num);
+
+        System.out.println("Input: " + num);
+        System.out.println("Roman Numeral: " + result);
+    }
+}
